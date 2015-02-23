5e878a17aa986b4d4705c5d0e7369c6967a390e3
ariel <ariel_556@hotmail.com>
2015-02-23 09:56:25 -0700

    first commit

---
+++ kiloton/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  kiloton
+//
+//  Created by Ariel Robles on 2/23/15.
+//  Copyright (c) 2015 nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
