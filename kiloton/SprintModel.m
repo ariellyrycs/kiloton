1ae99f5d81282813e34dd07415f767543d4a316f
ariellyrycs <ariel_556@hotmail.com>
2015-03-11 17:36:51 -0700

    adding imageURL to sprintModel

--- kiloton/SprintModel.m
+++ kiloton/SprintModel.m
@@ -2,18 +2,20 @@
 //  SprintModel.m
 //  kiloton
 //
-//  Created by Ariel Robles on 3/1/15.
+//  Created by Ariel Robles on 3/11/15.
 //  Copyright (c) 2015 nearsoft. All rights reserved.
 //
 
 #import "SprintModel.h"
 #import "InteractionsModel.h"
+#import "UserModel.h"
 
 
 @implementation SprintModel
 
 @dynamic currentDate;
 @dynamic currentWeight;
+@dynamic imageURL;
 @dynamic lastDate;
 @dynamic weightObjective;
 @dynamic eachInteraction;
